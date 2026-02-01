795dd9ea076d7f9dc8259a8ee05f8d914f361a6d
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-02-01 15:15:04 +0300

    add selection logic

--- server/program.cs
+++ server/program.cs
@@ -3,15 +3,25 @@ class Program
 {
     public static void Main(String[] args)
     {
-        Console.WriteLine("Меню:");
-        Console.WriteLine("1 -- Показать ФИО");
-        Console.WriteLine("2 -- Показать группу");
-        Console.WriteLine("3 -- Показать дату");
-        Console.WriteLine("4 -- Выход");
-        
+        bool flag = true;
         Console.WriteLine("Hello, client!");
-        Console.WriteLine("Lepilkin Maxim Aleksandrovich");
-        Console.WriteLine("ИСП-232");
-        Console.WriteLine(DateTime.Now);
+        while (flag == true)
+        {
+            Console.WriteLine("Меню:");
+            Console.WriteLine("1 -- Показать ФИО");
+            Console.WriteLine("2 -- Показать группу");
+            Console.WriteLine("3 -- Показать дату");
+            Console.WriteLine("4 -- Выход");
+            string a = Console.ReadLine();
+            switch (a)
+            {
+                case "1": Console.WriteLine("Lepilkin Maxim Aleksandrovich"); break;
+                case "2": Console.WriteLine("ИСП-232"); break;
+                case "3": Console.WriteLine(DateTime.Now); break;
+                case "4": flag = false; break;
+            }
+            Console.WriteLine();
+        }
     }
 }
+
